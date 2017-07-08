d3ffefe0fb9b939217e7a90d0bdaa59986956c31
liangxuCHEN <chenliangxu68@gmail.com>
2017-07-08 15:18:23 +0800

    fix bug mrq

--- myApi/tasks/package.py
+++ myApi/tasks/package.py
@@ -148,7 +148,6 @@ class ProductRate(BaseTask):
 
         if res['error']:
             log.info(res['info'])
-            update_mix_status(guid=res_check['row_id'], status=my_settings.CALC_ERROR_STATUS)
         else:
             try:
                 project_id = create_project(res, params.get("data"), params.get("filename"))
